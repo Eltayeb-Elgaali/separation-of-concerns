ce698e0b43364c585e8e7b3d8f74a05cdea388f6
eltayeb <gaaliii@yahoo.com>
2020-08-02 10:03:15 +0200

    pass-the-tests-2

--- isolate/04-element-children/exercises/pass-the-tests-2.js
+++ isolate/04-element-children/exercises/pass-the-tests-2.js
@@ -9,7 +9,9 @@ divEl.innerHTML = `
 console.log(divEl.nodeName, divEl.cloneNode(true));
 
 // --- write some code ---
+divEl.children[0].setAttribute('href','#top');
 
+divEl.children[0].children[0].innerHTML = 'to the top';
 
 
 
